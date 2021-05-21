912cbdc6fb801f1882f5dc9a60a2463e0cf3ebc6
MartinTrappenburg <martintrap1@gmail.com>
2021-05-21 19:22:26 +0200

    initial and final commit
    first and final commit cuz I forgot to commit throughout.

---
+++ gamedev 4/Scripts/EnemyX.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyX : MonoBehaviour {
+    public float speed;
+    private GameObject playerGoal;
+    private Rigidbody enemyRb;
+
+    void Start() {
+        enemyRb = GetComponent<Rigidbody>();
+        playerGoal = GameObject.Find("Player Goal");
+    }
+    void Update()
+    {
+        Vector3 lookDirection = (playerGoal.transform.position - transform.position).normalized;
+        enemyRb.AddForce(lookDirection * speed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter(Collision other) {
+        //yeet enemy if it hits a goal
+        if (other.gameObject.name == "Enemy Goal") {
+            Destroy(gameObject);
+        } 
+        else if (other.gameObject.name == "Player Goal") {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ gamedev 4/Scripts/PlayerControllerX.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour {
+    private Rigidbody playerRb;
+    private float speed = 525;
+    private float boostPower = 1.1;
+    private GameObject focalPoint;
+    private ParticleSystem particleSystem;
+    public bool hasPowerup;
+    public GameObject powerupIndicator;
+    public int powerUpDuration = 8;
+    private float normalStrength = 11;
+    private float powerupStrength = 27;
+    
+    void Start() {
+        playerRb = GetComponent<Rigidbody>();
+        particleSystem = GetComponentInChildren<ParticleSystem>();
+        focalPoint = GameObject.Find("Focal Point");
+    }
+
+    void Update() {
+        float verticalInput = Input.GetAxis("Vertical");
+        bool boostInput = Input.GetButton("Boost");
+        playerRb.AddForce(focalPoint.transform.forward * verticalInput * speed * Time.deltaTime);
+        if (boostInput)
+        {
+            playerRb.AddForce(focalPoint.transform.forward * boostPower, ForceMode.Impulse);
+            particleSystem.Play();
+        }
+        powerupIndicator.transform.position = transform.position + new Vector3(0, -0.6f, 0);
+    }
+
+    //powerup pickujp
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.CompareTag("Powerup")) {
+            Destroy(other.gameObject);
+            hasPowerup = true;
+            powerupIndicator.SetActive(true);
+            StartCoroutine(PowerupCooldown());
+        }
+    }
+
+    IEnumerator PowerupCooldown() {
+        yield return new WaitForSeconds(powerUpDuration);
+        hasPowerup = false;
+        powerupIndicator.SetActive(false);
+    }
+    //player absolutely BONKS the shit out of something
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Enemy")) {
+            Rigidbody enemyRigidbody = other.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer =  other.gameObject.transform.position - transform.position;
+            if (hasPowerup) {
+                enemyRigidbody.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
+            } else 
+            {
+                enemyRigidbody.AddForce(awayFromPlayer * normalStrength, ForceMode.Impulse);
+            }
+        }
+    }
+}
